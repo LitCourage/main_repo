a4c960dd7fcd0435bbf19f94ee94667f2979459a
FIRST_NAME LAST_NAME <LitCourage>
2024-01-24 17:25:01 +0300

    s

--- test_file.py
+++ test_file.py
@@ -1,1 +1,6 @@
-print('smth')
+class Game:
+    def __init__(self, name, creator, pushdate, mode=False):
+        self.name = name
+        self.creator = creator
+        self.pushdate = pushdate
+        
